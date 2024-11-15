03a1a707686d5b9b5063e644e27cf9719fabe75a
Bert de Bruijn <lpdebruijn@gmail.com>
2024-11-15 14:15:13 -0400

    Fix wrong template output string

--- prac/template/main.R
+++ prac/template/main.R
@@ -9,8 +9,7 @@ for (run_example in c(TRUE,FALSE)) {
   input <- readr::read_lines(paste0(folder, filename))
 
   # Write solution here
-  # ..
 
   # Output number depending on who wins
-  print(paste0("Solution for ", filename, ": ", ifelse(locs[1] >= dim * dim, i_move, 2 * i_move)))
+  print(paste0("Solution for ", filename, ": ", "TODO"))
 }
