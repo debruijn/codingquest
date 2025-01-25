7839bb70aa9d58c73006dbc230a5fcfb0e3daa29
Bert de Bruijn <lpdebruijn@gmail.com>
2025-01-25 16:23:52 -0400

    Clean-up of solution to 2022/04

--- 2022/04/main.js
+++ 2022/04/main.js
@@ -16,33 +16,8 @@ let input = fs.readFileSync(filename, {encoding: 'utf8', flag: 'r'});
 input = input.split('\n').map(row => row.split('').map(el => parseInt(el)));
 let wins = [0, 0, 0];
 
-
-
-
-// Utility function to check grid
-let checkPlayer = function(grid, player) {
-    let player_coords = [];
-    for (let i = 0; i < 7; i++) {
-        for (let j = 0; j < grid[i].length; j++) {
-            if (grid[i][j] === player) {
-                player_coords.push([i, j]);
-            }
-        }
-    }
-
-
-}
-
-
-let checkWinner = function(grid, col) {
-    let player = grid[col].at(-1);
-    // Check vertically
-    if (grid[col].slice(-4, -1).filter(x => x === player).length >= 3) {
-        return true;
-    }
-    // Check horizontally
-    let height = grid[col].length - 1;
-    let run_horizontally = grid.map(x => x[height])
+let checkElements = function(arr, player) {
+    let run_diagonally1 = arr
         .map(x => x === player)
         .reduce((a, b) => {
             if (a === 4) {
@@ -53,56 +28,43 @@ let checkWinner = function(grid, col) {
                 return 0
             }
         }, 0);
-    if (run_horizontally === 4) {
-        return true
+    return run_diagonally1 === 4;
+}
+
+let checkWinner = function(grid, col) {
+    let player = grid[col].at(-1);
+
+    // Check vertically
+    if (grid[col].slice(-4, -1).filter(x => x === player).length >= 3) {
+        return true;
     }
 
-    // Check diagonally
+    // Check horizontally
+    let height = grid[col].length - 1;
+    let horizontal = grid.map(x => x[height])
+    if (checkElements(horizontal, player)) {
+        return true;
+    }
+
+    // Check diagonally - first direction
     let diagonal = [];
     for (let i = -6; i <= 7; i++) {
         if ((col + i >= 0) && (col + i < 7)) {
             diagonal.push(grid[col + i][height + i]);
         }
     }
-    let run_diagonally1 = diagonal
-        .map(x => x === player)
-        .reduce((a, b) => {
-            // console.log(a, b);
-            if (a === 4) {
-                return 4
-            } else if (b) {
-                return a + 1
-            } else {
-                return 0
-            }
-        }, 0);
-    if (run_diagonally1 === 4) {
-        return true
+    if (checkElements(diagonal, player)) {
+        return true;
     }
 
+    // Check diagonally - other direction
     diagonal = [];
     for (let i = -6; i <= 7; i++) {
         if ((col + i >= 0) && (col + i < 7)) {
             diagonal.push(grid[col + i][height - i]);
         }
     }
-    let run_diagonally2 = diagonal
-        .map(x => x === player)
-        .reduce((a, b) => {
-            // console.log(a, b);
-            if (a === 4) {
-                return 4
-            } else if (b) {
-                return a + 1
-            } else {
-                return 0
-            }
-        }, 0);
-    if (run_diagonally2 === 4) {
-        return true
-    }
-
-    return false;
+    return checkElements(diagonal, player);
 }
 
 
